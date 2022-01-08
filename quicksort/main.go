c7fb344c83e4582a59e41cd33b06791e059070ae
Gabriel Magalhães <magalhaes.gabrielf@gmail.com>
2022-01-08 00:04:43 -0300

    feat: add quicksort count algorithm

--- quicksort/main.go
+++ quicksort/main.go
@@ -6,6 +6,7 @@ func main() {
 	numbers := []int{3, 5, 9, 133, 44, 20, 1, 7}
 
 	fmt.Printf("Result of the sum: %v\n", sumOf(numbers))
+	fmt.Printf("Result of the count: %v\n", countTotalItemsIn(numbers))
 }
 
 func sumOf(numbers []int) int {
@@ -13,5 +14,13 @@ func sumOf(numbers []int) int {
 		return numbers[0]
 	} else {
 		return numbers[0] + sumOf(numbers[1:])
+	}
+}
+
+func countTotalItemsIn(numbers []int) int {
+	if len(numbers) == 1 {
+		return 1
+	} else {
+		return 1 + countTotalItemsIn(numbers[1:])
 	}
 }
